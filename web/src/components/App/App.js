857131f9df7064bb9a7602bddcaa9ba30e6f4885
newsomderek <newsom.derek@gmail.com>
2018-05-10 22:12:21 -0700

    broken WIP auth0 implementation

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import Auth from '../Auth';
 
 class App extends Component {
     render() {
+        const auth = new Auth();
+        auth.login();
+
         return (
             <div className="App">
                 App body here
--- web/src/index.js
+++ web/src/index.js
@@ -4,6 +4,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 import App from './components/App/App';
 
+import '../node_modules/auth0-js/build/auth0.min';
+
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
